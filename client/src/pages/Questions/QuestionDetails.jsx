e2ccaba6d72991fb282f9cb4db7eafa839abe5f6
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2023-05-20 11:44:08 +0530

    added backend url

--- client/src/pages/Questions/QuestionDetails.jsx
+++ client/src/pages/Questions/QuestionDetails.jsx
@@ -21,7 +21,7 @@ const QuestionDetails = () => {
   const dispatch = useDispatch()
   const location = useLocation()
 
-  const url = 'http://localhost:3000'
+  const url = 'https://stack-overflow-clone-withmodifications.onrender.com'
   const[Answer, setAnswer] = useState('')
   const User = useSelector((state)=> (state.currentUserReducer))
 
